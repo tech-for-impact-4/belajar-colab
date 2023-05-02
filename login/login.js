e3e9395d947e86b68aebf35ed7aff6fb8ca364c7
auzanassdq <auzanassidqi@gmail.com>
2023-05-02 10:11:55 +0700

    compare input user login with data API

--- login/login.js
+++ login/login.js
@@ -1,3 +1,34 @@
+let users = []
+
+let inputLogin = {
+  username: "auzan",
+  password: "123"
+}
+
+// ============== async await =====================
+const getDataUser = async () => {
+  const response = await fetch("https://643e1624c72fda4a0bed5b7f.mockapi.io/user")
+  const result = await response.json()
+  
+  console.log(result);
+  users = [...result]
+
+  for (item of users) {
+    if (item.username === inputLogin.username && item.password === inputLogin.password){
+      console.log("login berhasil");
+      break
+    } else {
+      console.log("username dan password salah");
+      break
+    }
+  }
+}
+
+getDataUser()
+
+
+
+
 // ================ promise ======================
 fetch("https://643e1624c72fda4a0bed5b7f.mockapi.io/user")
 .then((response) => response.json())
@@ -8,13 +39,3 @@ fetch("https://643e1624c72fda4a0bed5b7f.mockapi.io/user")
 .catch((error) => {
   console.log(error);
 })
-
-// ============== async await =====================
-const getDataUser = async () => {
-  const response = await fetch("https://643e1624c72fda4a0bed5b7f.mockapi.io/user")
-  const result = await response.json()
-
-  console.log(result);
-}
-
-getDataUser()
